4faca938b5a02b117ce00accc2369b72bb60d89a
Igor Sena <igorsenamarques97@gmail.com>
2022-10-08 19:35:32 -0300

    [ALTER] existent user when register messange and remove yarn

--- src-backend/controllers/authController.js
+++ src-backend/controllers/authController.js
@@ -1,25 +1,17 @@
 const express = require('express');
-const bcrypt = require('bcryptjs')
-const User = require('../models/User')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const router = express.Router();
-
-async function authenticate({ email, password }) {
-  const searchLogin = await User.findOne({ email });
-  if (!searchLogin || !bcrypt.compareSync(password, searchLogin.password)) {
-      return false;
-  }
-  else {
-      return true;
-  }
-}
 
 router.post('/searchLogin', async (req, res) => {
   const searchLogin = await User.findOne({ where : { email : req.body.email }});
   if(searchLogin){
      const password_valid = await bcrypt.compare(req.body.password,searchLogin.password);
      if (password_valid){
-      res.status(200).send(searchLogin)
+      token = jwt.sign(req.body);
+      res.status(200).send(token)
      }
      else{
       res.status(400).send({ error: "Senha incorreta" })
--- src-backend/controllers/userController.js
+++ src-backend/controllers/userController.js
@@ -5,7 +5,11 @@ const User = require('../models/User')
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+  const { email } = req.body.email;
   try {
+    if (await User.findOne({ email })){
+      return res.status(400).send({ error: "Usuário já existente" });
+    }
     const createUser = await User.create(req.body)
     res.send({ createUser });
   } catch(err) {

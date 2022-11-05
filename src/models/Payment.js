3c83069a57fba00c43b1490951038b567402b3c4
Igor Sena <igorsenamarques97@gmail.com>
2022-11-05 03:45:15 -0300

    [ADD] payment table

---
+++ src/models/Payment.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+const db = require('../database/index.js');
+const User = require('./User.js');
+
+const Payment = db.define('payment', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    unique: true,
+    primaryKey: true,
+  },
+  dueDate: {
+    type: Sequelize.DATETIME,
+    allowNull: false,
+  },
+  paid: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  },
+  id_user: {
+    type: Sequelize.INTEGER,
+    required: true,
+    allowNull: false,
+    references: {
+      model: 'users',
+      key: 'id',
+    },
+  },
+});
+
+User.hasMany(Payment);
+Payment.belongsTo(User);
+
+Payment.sync();
+
+module.exports = Payment;

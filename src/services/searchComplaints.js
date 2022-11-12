ece5c19c8ec7bd6b074d2567b3bab18c0cf05470
Igor Sena <72056234+igormarques1170670@users.noreply.github.com>
2022-11-12 18:44:15 -0300

    [ALTER] search all complaints instead by status

--- src/services/searchComplaints.js
+++ src/services/searchComplaints.js
@@ -3,12 +3,11 @@ const Complaint = require('../models/Complaint');
 
 const router = express.Router();
 
-router.post('/findAllUnresolvedComplaints', async (req, res) => {
+router.post('/findAllComplaints', async (req, res) => {
   try {
     const searchUnresolvedComplaints = await Complaint.findAll({
       where: {
         id_condominium: req.body.id_condominium,
-        resolved: false,
       },
       attributes: { exclude: ['userId'] },
       order: [['createdAt', 'ASC']],
@@ -17,26 +16,6 @@ router.post('/findAllUnresolvedComplaints', async (req, res) => {
       res.status(204).send({ error: 'Não possuem reclamações registradas' });
     } else {
       res.status(200).send(searchUnresolvedComplaints);
-    }
-  } catch (err) {
-    res.status(400).send({ error: 'Falha ao buscar as reclamações' });
-  }
-});
-
-router.post('/findAllResolvedComplaints', async (req, res) => {
-  try {
-    const searchResolvedComplaints = await Complaint.findAll({
-      where: {
-        id_condominium: req.body.id_condominium,
-        resolved: true,
-      },
-      attributes: { exclude: ['userId'] },
-      order: [['createdAt', 'DESC']],
-    });
-    if (searchResolvedComplaints == 0) {
-      res.status(204).send({ error: 'Não possuem reclamações registradas' });
-    } else {
-      res.status(200).send(searchResolvedComplaints);
     }
   } catch (err) {
     res.status(400).send({ error: 'Falha ao buscar as reclamações' });
